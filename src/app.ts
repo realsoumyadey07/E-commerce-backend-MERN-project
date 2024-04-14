32e52b6f5cb07771d1b8c230ca409dfce4112ba2
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-04-14 11:58:57 +0530

    user controller added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 
+//importing routes
+
+import userRoute from './routes/user.js';
+
 const app = express();
 const port = 3000;
+
+//using routes
+
+app.use("/api/v1", userRoute);
 
 app.listen(port, ()=> {
     console.log(`Server is connected to ${port}`);
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 import validator from "validator";
 
+interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  photo: string;
+  role: "admin" | "user";
+  gender: "male" | "female";
+  dob: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  // virtual attribute
+  age: number;
+}
+
 const userShema = new mongoose.Schema(
   {
     _id: {
@@ -45,6 +59,11 @@ userShema.virtual("age").get(function (){
   const today = new Date();
   const dob = this.dob;
   let age = today.getFullYear() - dob.getFullYear();
+
+  if (today.getMonth() < dob.getMonth() || today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate()){
+    age--;
+  }
+  return age;
 })
 
-export const User = mongoose.model("User", userShema);
+export const User = mongoose.model<IUser>("User", userShema);
