bebf035b76f13704df1b4a6ee074982b5510080f
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-04-14 12:16:40 +0530

    user create router added

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,19 +1,29 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.js";
+import { NewUserRequestBody } from "../types/types.js";
 
-export const newUser = async(
-    req: Request, 
-    res: Response, 
-    next: NextFunction) => {
-        try {
-            const {} = req.body;
-            const user = await User.create({});
+export const newUser = async (
+  req: Request<{}, {}, NewUserRequestBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, photo, gender, role, _id, dob } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      photo,
+      gender,
+      role,
+      _id,
+      dob,
+    });
 
-            res.status(200).json({
-                success: true,
-                message: `Welcome, ${user.name}`
-            });
-        } catch (error) {
-            
-        }
-    };
+    res.status(200).json({
+      success: true,
+      message: `Welcome, ${user.name}`,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
